c95683ab259a1e5c73023d2c3fd26c35cea8d47e
Ibragim <ballismlife@gmail.com>
2019-01-14 12:57:04 +0500

    update

--- marmelad/data.marmelad.js
+++ marmelad/data.marmelad.js
@@ -18,5 +18,31 @@ module.exports = {
       'danger',
     ],
   },
+  deviceL1S1 : [
+    {
+      "img" : "img/device.svg",
+      "name" : "Mobile",
+      "desc" : "Get notifications about new releases in our mobile app.",
+      "cur" : "$",
+      "price" : "10",
+      "month" : "/ month"
+    },
+    {
+      "img" : "img/laptop.svg",
+      "name" : "Desktop",
+      "desc" : "Enjoy new episodes on your laptop in browser with our web service, which supports all the platforms.",
+      "cur" : "$",
+      "price" : "15",
+      "month" : "/ month"
+    },
+    {
+      "img" : "img/monitor.svg",
+      "name" : "Tv",
+      "desc" : "Watch your favorite series at home on large screen with our TV application.",
+      "cur" : "$",
+      "price" : "20",
+      "month" : "/ month"
+    }
+  ],
   pageTitle: 'marmelad',
 };

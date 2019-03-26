d9355b89939aa9e488bbdde3e1afe1af18b11936
Alimjanov-Ibragim <ballismlife@gmail.com>
2019-03-26 15:01:15 +0500

    адаптировал

--- marmelad/_blocks/loop3Step8Accordion/loop3Step8Accordion.jquery.js
+++ marmelad/_blocks/loop3Step8Accordion/loop3Step8Accordion.jquery.js
@@ -1,16 +1,18 @@
 var Atitle = $('.loop3Step8Accordion__title');
 var Abody = $('.loop3Step8Accordion__body');
 
-$('.loop3Step8Accordion__title').on('click', function () {
-    var body = $(this).closest('.loop3Step8Accordion__item').find('.accordion__body');
+Atitle.on('click', function(e){
+    e.preventDefault();
 
-    if ($(this).closest('.loop3Step8Accordion__item').hasClass('isOpened')) {
-      $(this).closest('.loop3Step8Accordion__item').removeClass('isOpened');
-      body.stop().slideUp();
-    } else {
-      $('.loop3Step8Accordion__item.isOpened .accordion__body').slideUp();
-      $('.loop3Step8Accordion__item.isOpened').removeClass('isOpened');
-      $(this).closest('.loop3Step8Accordion__item').addClass('isOpened');
-      body.stop().slideDown();
-    }
+    const index = $(this).index();
+
+    Atitle
+        .eq(index)
+        .addClass('isOpened')
+        .siblings().removeClass('isOpened');
+
+    Abody
+        .eq(index)
+        .addClass('isOpened')
+        .siblings().removeClass('isOpened');
 });
--- static/js/app.js
+++ static/js/app.js
@@ -129,18 +129,11 @@ $(function () {
 
   var Atitle = $('.loop3Step8Accordion__title');
   var Abody = $('.loop3Step8Accordion__body');
-  $('.loop3Step8Accordion__title').on('click', function () {
-    var body = $(this).closest('.loop3Step8Accordion__item').find('.accordion__body');
-
-    if ($(this).closest('.loop3Step8Accordion__item').hasClass('isOpened')) {
-      $(this).closest('.loop3Step8Accordion__item').removeClass('isOpened');
-      body.stop().slideUp();
-    } else {
-      $('.loop3Step8Accordion__item.isOpened .accordion__body').slideUp();
-      $('.loop3Step8Accordion__item.isOpened').removeClass('isOpened');
-      $(this).closest('.loop3Step8Accordion__item').addClass('isOpened');
-      body.stop().slideDown();
-    }
+  Atitle.on('click', function (e) {
+    e.preventDefault();
+    var index = $(this).index();
+    Atitle.eq(index).addClass('isOpened').siblings().removeClass('isOpened');
+    Abody.eq(index).addClass('isOpened').siblings().removeClass('isOpened');
   });
   $('.loop3Step8Slider').owlCarousel({
     loop: true,
